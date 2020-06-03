f87a7bbde12dd8727a7671195c06175d49e69e13
Ken Williams <kenwilliams74@gmail.com>
2020-06-03 14:00:14 -0700

    Controller is completed.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,40 +1,30 @@
 var express = require("express");
+var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-var burger = require("../models/burger.js");
-
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    res.render("index", { burgers: data });
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name"
-  ], [
-    req.body.name
-  ], function(result) {
-    
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      
+  burger.updateOne({ devoured: req.body.devoured }, condition, function (
+    result
+  ) {
+    if (result.changedRows === 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -42,12 +32,10 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      
+  burger.deleteOne(condition, function (result) {
+    if (result.affectedRows === 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -55,5 +43,4 @@ router.delete("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
